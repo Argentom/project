4b6f95ef6a01b73237012a3a70d748638f619fde
Argentom <152075079+Argentom@users.noreply.github.com>
2024-05-09 19:37:44 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,9 @@
+import requests
+
+def create_new_token():
+    """Создание нового токена"""
+    metadata_url = "http://169.254.169.254/computeMetadata/v1/instance/service-accounts/default/token"
+    headers = {"Metadata-Flavor": "Google"}
+    response = requests.get(metadata_url, headers=headers)
+    return response.json()
+print(create_new_token())
