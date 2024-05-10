9eaf20e3c53a6352ec408ad827f769293dfd5cbe
Argentom <152075079+Argentom@users.noreply.github.com>
2024-05-10 10:37:58 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,21 @@
+
+token = '7157762135:AAH35Jx1bQKivwXzXSUiOeDIT3-eHHSitN4'
+
+hello_text = 'привет,  это бот, который по команде /tts принимает текстовые сообщения и преобразовывает их в аудио,  а по команде /stt голосовые собщения преобразовывает в текстовые. Также есть команда /chatGPT c помощью которой вы можете задать вопрос нейросети.'
+help_text = 'Что такое /tts? От английского Text to speech означает из текста в речь.' \
+            ' Вы присылаете текст, а нейросеть озвучивает его.' \
+            ' /stt с английского из речи в текст, вы присылаете голосовое сообщение, а оно преобразуется в текст.' \
+            ' /chatGPT это нейросеть которая может помочь тебе ответить на вопросы'
+GPT_MODEL='yandexgpt'
+LOGS_PATH = 'errors.txt'
+MAX_TOKENS=1500
+MAX_GPT_TOKENS=350
+ADMIN_ID=989552988
+FOLDER_ID = 'b1g22vjohjr4f14esoem'#'b1gv7tv6tfmd502i7fif'
+MAX_USER_TTS_SYMBOLS = 600
+MAX_TTS_SYMBOLS = 500
+MAX_USER_STT_BLOCKS=15
+
+
+DB_NAME = "speech_kit.db"
+DB_TABLE_USERS_NAME = "messages"
